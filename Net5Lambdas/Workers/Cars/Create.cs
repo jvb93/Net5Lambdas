1a5c401966fba41461884d12880f499e01003c55
Justin Van Bibber <1976577+jvb93@users.noreply.github.com>
2021-02-14 17:29:53 -0800

    bugfixes, logging

--- Net5Lambdas/Workers/Cars/Create.cs
+++ Net5Lambdas/Workers/Cars/Create.cs
@@ -31,21 +31,29 @@ namespace Net5Lambdas.Workers.Cars
         /// <param name="request"></param>
         public async Task CreateCar(SQSEvent request, ILambdaContext context)
         {
-            context.Logger.LogLine("creating car\n");
-
-            if (_hostBuilder.TryBuild(context, () => new LambdaStartup(), out var serviceProvider))
+            try
             {
-                var queueMessage = JsonSerializer.Deserialize<SqsMessageWrapper>(request.Records.First().Body);
-                using var container = serviceProvider.CreateScope();
-                var taskService = container.ServiceProvider.GetRequiredService<ITaskService>();
+                context.Logger.LogLine("creating car\n");
 
-                await taskService.UpdateTaskStatusAsync(queueMessage.TaskId, ServiceTaskStatus.InProgress);
-                //simulate 5 second database latency
-                await Task.Delay(5000);
+                if (_hostBuilder.TryBuild(context, () => new LambdaStartup(), out var serviceProvider))
+                {
+                    var queueMessage = JsonSerializer.Deserialize<SqsMessageWrapper>(request.Records.First().Body);
+                    using var container = serviceProvider.CreateScope();
+                    var taskService = container.ServiceProvider.GetRequiredService<ITaskService>();
 
-                await taskService.UpdateTaskStatusAsync(queueMessage.TaskId, ServiceTaskStatus.Complete,
-                    Guid.NewGuid());
+                    await taskService.UpdateTaskStatusAsync(queueMessage.TaskId, ServiceTaskStatus.InProgress);
+                    //simulate 5 second database latency
+                    await Task.Delay(5000);
+
+                    await taskService.UpdateTaskStatusAsync(queueMessage.TaskId, ServiceTaskStatus.Complete,
+                        Guid.NewGuid());
+                }
             }
+            catch (Exception ex)
+            {
+                context.Logger.LogLine(ex.Message);
+            }
+          
         }
     }
 }
